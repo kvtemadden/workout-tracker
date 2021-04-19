1ddf3d06dea345f1de5db6cb55afc745c44d3fc9
kate <katy.madden99@gmail.com>
2021-04-19 19:20:57 +0100

    moved routes to different folder

--- server.js
+++ server.js
@@ -1,37 +1,22 @@
 const express = require("express");
-const mongojs = require("mongojs");
+const mongoose = require("mongoose");
 const logger = require("morgan");
-const app = express();
-const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const app = express();
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-const databaseUrl = "workoutTracker";
-const collections = ["workouts"];
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", { useNewUrlParser: true });
 
-const db = mongojs(databaseUrl, collections);
-
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/exercise.html'));
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/stats.html'));
-});
+app.use(require("./routes/workoutAPI.js"));
+app.use(require("./routes/viewsHTML.js"));
 
 app.listen(PORT, () => {
-  console.log(`Workout Tracker running on port ${PORT}!`);
+  console.log(`Workout tracker running on port http://localhost:${PORT}!`);
 });

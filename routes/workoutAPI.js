8d36ef36e7ec914f56febefdd9f5ae09f5bbc319
kate <katy.madden99@gmail.com>
2021-04-22 21:50:08 +0100

    fixed link to models folder

--- routes/workoutAPI.js
+++ routes/workoutAPI.js
@@ -1,4 +1,39 @@
 const router = require("express").Router();
+const Workout = require("../models").Workout;
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(workouts => res.json(workouts))
+    .catch(err => res.json(err));
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({
+    day: Date.now()
+  })
+    .then(newWorkout => {
+      res.json(newWorkout);
+    })
+    .catch(err => res.json(err));
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true }
+    )
+      .then(workout => res.json(workout))
+      .catch(err => res.json(err));
+  });
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => res.json(err));
+});
 
 
 module.exports = router;
